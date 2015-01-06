fa0625a1a94d3adabdfe5a641991649f09807086
ColinLiiii <guanyu87.li@gmail.com>
2015-01-06 18:27:50 -0500

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,26 +1,32 @@
+#Read data from .txt
 data<-read.table("household_power_consumption.txt",
                   header=T,
                   sep=";",
                   na.strings="?")
 
+#Convert the Date and Time variables to Date/Time classes in R
 data$Date=as.Date(data$Date,
                   format="%d/%m/%Y")
                   
 data$Time<-strptime(data$Time,
                     format="%H:%M:%S")
                     
+#Convert Global_active_power from factor into numeric
 data$Global_active_power<-as.numeric(
                                       as.character(
                                                     data$Global_active_power
                                                     )
                                     )
+#We will only be using data from the dates 2007-02-01 and 2007-02-02
 subdata <- subset(data, 
                       data$Date >= ("2007-02-01") & data$Date <= as.Date("2007-02-02"))
-                      
+ 
+#Make the plot                     
 hist(subdata$Global_active_power,
       col="red",
       xlab="Global Active Power(kilowatts)",
       main="Global Active Power")
-      
+
+#Save the plot      
 dev.copy(png,file="plot1.png")
 dev.off()
